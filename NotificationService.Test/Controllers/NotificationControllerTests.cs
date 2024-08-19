932d1450bca62bdfc7b67d8f3b7cd28c510f2a65
Daniel Tavares Penkal dos Santos <dani.tavares.penkal@gmail.com>
2024-08-18 23:02:36 -0300

    feat: added field Validations to NotificationRequest using FluentValidations library; Added and adapted unit tests related to that new implementation.

--- NotificationService.Test/Controllers/NotificationControllerTests.cs
+++ NotificationService.Test/Controllers/NotificationControllerTests.cs
@@ -1,4 +1,6 @@
 using AutoBogus;
+using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NotificationService.Application.Abstractions.UseCases;
@@ -13,24 +15,29 @@ namespace NotificationService.Test.Controllers;
 public class NotificationControllerTests
 {
         private readonly Mock<INotificationProcessor> _mockNotificationProcessor;
+        private readonly Mock<IValidator<NotificationRequest>> _mockValidatorRequest;
         private readonly NotificationController _controller;
 
         public NotificationControllerTests()
         {
             _mockNotificationProcessor = new Mock<INotificationProcessor>();
-            _controller = new NotificationController(_mockNotificationProcessor.Object);
+            _mockValidatorRequest = new Mock<IValidator<NotificationRequest>>();
+            
+            _controller = new NotificationController(_mockNotificationProcessor.Object, _mockValidatorRequest.Object);
         }
 
         [Fact]
         public async Task SendNotification_ShouldReturnOk_WhenProcessorReturnsSuccess()
         {
             // Arrange
-            var request = AutoFaker.Generate<NotificationRequest>(); 
+            var request = CreateRequest();
             var limitType = LimitType.RateLimited; 
             var result = Result.Success();
 
             _mockNotificationProcessor.Setup(p => p.Send(It.IsAny<Notification>(), It.IsAny<CancellationToken>()))
                                        .ReturnsAsync(result);
+            _mockValidatorRequest.Setup(x => x.ValidateAsync(It.IsAny<NotificationRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ValidationResult());
 
             // Act
             var actionResult = await _controller.SendNotification(request, limitType, CancellationToken.None);
@@ -44,12 +51,15 @@ public class NotificationControllerTests
         public async Task SendNotification_ShouldReturnRateLimitExceeded_WhenProcessorReturnsFailure()
         {
             // Arrange
-            var request = AutoFaker.Generate<NotificationRequest>();
+            var request = CreateRequest();
             var limitType = LimitType.RateLimited;
             var result = Result.Failure(new Error("RateLimit.Exceeded", "Rate limit exceeded"));
 
             _mockNotificationProcessor.Setup(p => p.Send(It.IsAny<Notification>(), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(result);
+            
+            _mockValidatorRequest.Setup(x => x.ValidateAsync(It.IsAny<NotificationRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ValidationResult());
 
             // Act
             var actionResult = await _controller.SendNotification(request, limitType, CancellationToken.None);
@@ -57,5 +67,45 @@ public class NotificationControllerTests
             // Assert
             var statusCodeResult = Assert.IsType<ObjectResult>(actionResult);
             Assert.Equal(429, statusCodeResult.StatusCode);
+        }
+        
+        [Fact]
+        public async Task SendNotification_ShouldReturnBadRequest_WhenRequestIsNotValid()
+        {
+            // Arrange
+            var request = CreateRequest();
+            var limitType = LimitType.Unlimited; 
+            var result = Result.Success();
+
+            _mockNotificationProcessor.Setup(p => p.Send(It.IsAny<Notification>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(result);
+            _mockValidatorRequest.Setup(x => x.ValidateAsync(It.IsAny<NotificationRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ValidationResult(new List<ValidationFailure>
+                {
+                    Capacity = 1
+                }));
+
+            // Act
+            var actionResult = await _controller.SendNotification(request, limitType, CancellationToken.None);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(actionResult);
+            Assert.Equal(result, okResult.Value);
+        }
+
+        private NotificationRequest CreateRequest()
+        {
+            var emailAddress = "dani.teste@gmail.com";
+
+            var recipient = new AutoFaker<Recipient>()
+                .RuleFor(x => x.EmailAdress, _ => emailAddress);
+
+            
+            var request = new AutoFaker<NotificationRequest>()
+                .RuleFor(x => x.Message, _ => AutoFaker.Generate<Message>())
+                .RuleFor(x => x.Recipient, _ => recipient);
+            
+
+            return request;
         }
 }
